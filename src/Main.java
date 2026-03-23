85e3118b003883c2985783344240ca5b89a1a3c5
attachment4 <karavashkinivan1995@gmail.com>
2026-03-23 06:14:26 -0800

    Changed java syntax

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 13333;
+        int ticketPrice = 13_333;
         int milePerticket = 20;
         int bonusMiles = ticketPrice / milePerticket;
         System.out.println("Количество начисленных миль:" + bonusMiles);
